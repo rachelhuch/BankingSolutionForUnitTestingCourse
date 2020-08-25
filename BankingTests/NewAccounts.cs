ce4b67048c90184fabdc86db6793954db6d5c9e4
Jeffry Gonzalez <jeff@hypertheory.com>
2020-08-25 15:06:14 -0400

    Add project files.

---
+++ BankingTests/NewAccounts.cs
@@ -0,0 +1,26 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace BankingTests
+{
+    public class NewAccounts
+    {
+        [Fact]
+        public void NewAccountsHaveCorrectOpeningBalance()
+        {
+            // WTCYWYH (Write the code you wish you had)
+            // Given
+            var account = new BankAccount();
+
+            // When
+            decimal balance = account.GetBalance();
+
+            // Then
+            Assert.Equal(1000M, balance);
+        }
+    }
+}
